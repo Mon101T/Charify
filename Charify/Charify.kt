745f2858f81d4a071bab9dd3cf7626ad7c3031c9
Hanzwel21 <hanueldomingo21@gmail.com>
2025-05-05 22:04:00 +0800

    Add files via upload

---
+++ Charify/Charify.kt
@@ -0,0 +1,286 @@
+<View
+android:id="@+id/rectangle_4"
+android:layout_width="412dp"
+android:layout_height="917dp"
+android:layout_alignParentLeft="true"
+android:layout_alignParentTop="true"
+android:background="#000000"
+/>
+
+
+
+<View
+android:id="@+id/charify_log"
+android:layout_width="119dp"
+android:layout_height="119dp"
+android:layout_alignParentLeft="true"
+android:layout_marginLeft="153dp"
+android:layout_alignParentTop="true"
+android:layout_marginTop="79dp"
+android:background="@drawable/charify_log"
+android:elevation="4dp"
+/>
+
+<!-- drawable/charify_log.xml -->
+<vector
+xmlns:android="http://schemas.android.com/apk/res/android"
+xmlns:aapt="http://schemas.android.com/aapt"
+android:width="119dp"
+android:height="119dp"
+android:viewportWidth="119"
+android:viewportHeight="119"
+>
+<group>
+<clip-path
+android:pathData="M0 0H119V119H0V0Z"
+/>
+</group>
+</vector>
+
+
+<View
+android:id="@+id/charify_wor"
+android:layout_width="216dp"
+android:layout_height="121dp"
+android:layout_centerHorizontal="true"
+android:layout_alignParentTop="true"
+android:layout_marginTop="175dp"
+android:background="@drawable/charify_wor"
+android:elevation="4dp"
+/>
+
+<!-- drawable/charify_wor.xml -->
+<vector
+xmlns:android="http://schemas.android.com/apk/res/android"
+xmlns:aapt="http://schemas.android.com/aapt"
+android:width="216dp"
+android:height="121dp"
+android:viewportWidth="216"
+android:viewportHeight="121"
+>
+<group>
+<clip-path
+android:pathData="M0 0H216V121H0V0Z"
+/>
+</group>
+</vector>
+
+
+<View
+android:id="@+id/rectangle_4"
+android:layout_width="413dp"
+android:layout_height="687dp"
+android:layout_alignParentLeft="true"
+android:layout_alignParentTop="true"
+android:layout_marginTop="262dp"
+android:background="@drawable/rectangle_4"
+/>
+
+<!-- drawable/rectangle_4.xml -->
+<vector
+xmlns:android="http://schemas.android.com/apk/res/android"
+xmlns:aapt="http://schemas.android.com/aapt"
+android:width="413dp"
+android:height="687dp"
+android:viewportWidth="413"
+android:viewportHeight="687"
+>
+<group>
+<clip-path
+android:pathData="M0 0H413V687H0V0Z"
+/>
+<group
+android:translateX="1"
+android:translateY="0"
+android:pivotX="206.5"
+android:pivotY="343.5"
+android:scaleX="2"
+android:scaleY="2"
+android:rotation="90"
+>
+<path
+android:pathData="M0 0V687H413V0"
+>
+<aapt:attr name="android:fillColor">
+<gradient
+android:type="linear"
+android:startX="103.25"
+android:startY="343.5"
+android:endX="309.75"
+android:endY="343.5"
+>
+<item
+android:color="#00000000"
+android:offset="0"
+/>
+<item
+android:color="#D9D9D9"
+android:offset="1"
+/>
+</gradient>
+</aapt:attr>
+</path>
+</group>
+</group>
+</vector>
+
+
+<View
+android:id="@+id/rectangle_5"
+android:layout_width="327dp"
+android:layout_height="53dp"
+android:layout_centerHorizontal="true"
+android:layout_alignParentTop="true"
+android:layout_marginTop="749dp"
+android:background="@drawable/rectangle_5"
+/>
+
+<!-- drawable/rectangle_5.xml -->
+<vector
+xmlns:android="http://schemas.android.com/apk/res/android"
+xmlns:aapt="http://schemas.android.com/aapt"
+android:width="327dp"
+android:height="53dp"
+android:viewportWidth="327"
+android:viewportHeight="53"
+>
+<group>
+<clip-path
+android:pathData="M10 0H317C322.523 0 327 4.47715 327 10V43C327 48.5228 322.523 53 317 53H10C4.47715 53 0 48.5228 0 43V10C0 4.47715 4.47715 0 10 0Z"
+/>
+<path
+android:pathData="M0 0V53H327V0"
+android:fillColor="#5CCE61"
+/>
+</group>
+</vector>
+
+
+<View
+android:id="@+id/rectangle_4"
+android:layout_width="327dp"
+android:layout_height="53dp"
+android:layout_centerHorizontal="true"
+android:layout_alignParentTop="true"
+android:layout_marginTop="823dp"
+android:background="@drawable/rectangle_4"
+/>
+
+<!-- drawable/rectangle_4.xml -->
+<vector
+xmlns:android="http://schemas.android.com/apk/res/android"
+xmlns:aapt="http://schemas.android.com/aapt"
+android:width="327dp"
+android:height="53dp"
+android:viewportWidth="327"
+android:viewportHeight="53"
+>
+<group>
+<clip-path
+android:pathData="M10 0H317C322.523 0 327 4.47715 327 10V43C327 48.5228 322.523 53 317 53H10C4.47715 53 0 48.5228 0 43V10C0 4.47715 4.47715 0 10 0Z"
+/>
+<path
+android:pathData="M0 0V53H327V0"
+android:fillColor="#5CCE61"
+/>
+</group>
+</vector>
+
+
+<TextView
+android:id="@+id/log_in"
+android:layout_width="71dp"
+android:layout_height="53dp"
+android:layout_centerHorizontal="true"
+android:layout_alignParentTop="true"
+android:layout_marginTop="749dp"
+android:text="@string/log_in"
+android:textAppearance="@style/log_in"
+android:gravity="center_horizontal|top"
+/>
+<!--
+Font family: NATS
+Line height: 51sp
+-->
+
+<!-- styles.xml -->
+<style name="log_in">
+<item name="android:textSize">24sp</item>
+<item name="android:textColor">#000000</item>
+<item name="android:shadowColor">#40000000</item>
+<item name="android:shadowDx">0</item>
+<item name="android:shadowDy">4</item>
+<item name="android:shadowRadius">4</item>
+</style>
+
+<!-- strings.xml -->
+<string name="log_in">
+LOG IN
+</string>
+
+
+
+<TextView
+android:id="@+id/welcome"
+android:layout_width="210dp"
+android:layout_height="105dp"
+android:layout_centerHorizontal="true"
+android:layout_alignParentTop="true"
+android:layout_marginTop="338dp"
+android:text="@string/welcome"
+android:textAppearance="@style/welcome"
+android:gravity="center_horizontal|top"
+/>
+<!--
+Font family: NATS
+Line height: 106sp
+-->
+
+<!-- styles.xml -->
+<style name="welcome">
+<item name="android:textSize">50sp</item>
+<item name="android:textColor">#FFFFFF</item>
+<item name="android:shadowColor">#40FFFFFF</item>
+<item name="android:shadowDx">0</item>
+<item name="android:shadowDy">4</item>
+<item name="android:shadowRadius">4</item>
+</style>
+
+<!-- strings.xml -->
+<string name="welcome">
+WELCOME
+</string>
+
+
+
+<TextView
+android:id="@+id/create_an_a"
+android:layout_width="221dp"
+android:layout_height="50dp"
+android:layout_centerHorizontal="true"
+android:layout_alignParentTop="true"
+android:layout_marginTop="823dp"
+android:text="@string/create_an_a"
+android:textAppearance="@style/create_an_a"
+android:gravity="center_horizontal|top"
+/>
+<!--
+Font family: NATS
+Line height: 51sp
+-->
+
+<!-- styles.xml -->
+<style name="create_an_a">
+<item name="android:textSize">24sp</item>
+<item name="android:textColor">#000000</item>
+<item name="android:shadowColor">#40000000</item>
+<item name="android:shadowDx">0</item>
+<item name="android:shadowDy">4</item>
+<item name="android:shadowRadius">4</item>
+</style>
+
+<!-- strings.xml -->
+<string name="create_an_a">
+CREATE AN ACCOUNT
+</string>
+
